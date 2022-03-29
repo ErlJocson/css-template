b76d45ba6503372ec0f35e2b79c4e825fbc2fd9a
Erl Jocson <jocsonerl@gmail.com>
2022-03-29 19:30:01 +0800

    responsive nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,21 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 function Navigation() {
+  const [isShow, toggle] = useState(false);
   return (
     <>
       <NavigationBar>
-        <TitleLink to="/">Home</TitleLink>
-        <MyLink to="/cards">Cards</MyLink>
-        <MyLink to="/shadow">Shadow</MyLink>
-        <MyLink to="/button">Button</MyLink>
+        <NavigationContents>
+          <TitleLink to="/">Home</TitleLink>
+          <MyLink to="/cards">Cards</MyLink>
+          <MyLink to="/shadow">Shadow</MyLink>
+          <MyLink to="/button">Button</MyLink>
+        </NavigationContents>
+        <MenuIcon
+          onClick={() => {
+            toggle(!isShow);
+          }}
+        >
+          <Lines />
+          <Lines />
+          <Lines />
+        </MenuIcon>
+        <BurgerNav show={isShow}>
+          <TitleLink to="/">Home</TitleLink>
+          <MyLink to="/cards">Cards</MyLink>
+          <MyLink to="/shadow">Shadow</MyLink>
+          <MyLink to="/button">Button</MyLink>
+        </BurgerNav>
       </NavigationBar>
     </>
   );
 }
 
 export default Navigation;
+
+const MenuIcon = styled.div`
+  display: none;
+  @media screen and (max-width: 600px) {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    margin-top: 16px;
+    flex-direction: column;
+    cursor: pointer;
+  }
+`;
+
+const Lines = styled.div`
+  height: 5px;
+  margin: 2px 0;
+  background-color: black;
+  width: 30px;
+  border-radius: 10px;
+`;
 
 const MyLink = styled(Link)`
   margin: 6px 20px 0 20px;
@@ -42,7 +80,24 @@ const NavigationBar = styled.nav`
   top: 0;
   bottom: 0;
   left: 0;
+  width: 250px;
+
+  @media screen and (max-width: 600px) {
+    width: ${(props) => (props.show ? "250px" : "50px")};
+  }
+`;
+
+const NavigationContents = styled.div`
   display: flex;
   flex-direction: column;
-  width: 250px;
+
+  @media screen and (max-width: 600px) {
+    & {
+      display: none;
+    }
+  }
+`;
+
+const BurgerNav = styled(NavigationContents)`
+  display: ${(props) => (props.show ? "flex" : "none")};
 `;
--- src/components/pages/components/Container.js
+++ src/components/pages/components/Container.js
@@ -11,4 +11,9 @@ const MyContainer = styled.div`
   margin-left: 250px;
   width: calc(100% - 250px);
   min-height: calc(100vh - 10px);
+
+  @media screen and (max-width: 600px) {
+    margin-left: 50px;
+    width: calc(100% - 50px);
+  }
 `;
